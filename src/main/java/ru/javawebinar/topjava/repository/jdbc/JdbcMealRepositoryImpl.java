b3e7fd8fd038f19a3f5728d46857c6a6e1abec81
promoscow <slava_rossii@list.ru>
2017-08-06 22:52:17 +0300

    meals.sql

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
@@ -1,14 +1,38 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.sql.DataSource;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public class JdbcMealRepositoryImpl implements MealRepository {
+
+
+    private final JdbcTemplate jdbcTemplate;
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private final SimpleJdbcInsert insertMeal;
+
+    @Autowired
+    public JdbcMealRepositoryImpl(DataSource dataSource, JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.insertMeal = new SimpleJdbcInsert(dataSource)
+                .withTableName("meals")
+                .usingGeneratedKeyColumns("id");
+
+        this.jdbcTemplate = jdbcTemplate;
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
 
     @Override
     public Meal save(Meal meal, int userId) {
---
+++ src/main/resources/db/meals.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS meals;
+DROP SEQUENCE IF EXISTS meal_seq;
+
+CREATE SEQUENCE meal_seq START 100000;
+
+CREATE TABLE meals
+(
+  id            INTEGER PRIMARY KEY DEFAULT nextval('meal_seq'),
+  name          VARCHAR NOT NULL,
+  description   VARCHAR NOT NULL,
+  datetime      TIMESTAMP DEFAULT now(),
+  calories      INTEGER DEFAULT 200 NOT NULL,
+  userid        INTEGER NOT NULL
+);

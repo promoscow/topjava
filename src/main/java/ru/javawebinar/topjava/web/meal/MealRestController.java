5146bd34d442dd20da97d53d1342b8f146dee64d
promoscow <slava_rossii@list.ru>
2017-09-09 14:15:55 +0300

    added soapui tests

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,7 +1,17 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.Meal;
 
-@Controller
+@RestController
+@RequestMapping(MealRestController.REST_URL)
 public class MealRestController extends AbstractMealController {
+    static final String REST_URL = "/rest/meal";
+
+    // TODO: 09.09.17 Вернуть user в JSON-ответе
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Meal get(@RequestParam ("id") int id) {
+        return super.get(id);
+    }
 }
